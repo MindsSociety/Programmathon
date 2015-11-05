70b5b80a3407b57187672c23ecc0acc818e00051
Conor Fitzpatrick <aks47u@users.noreply.github.com>
2015-11-05 12:06:05 +0000

    formatting

--- solutions-2014/q1.py
+++ solutions-2014/q1.py
@@ -1,8 +1,8 @@
 n, k, q = map(int, raw_input().split())
 array = map(int, raw_input().split())
 front = 0
-
 front = (front - k) % n
+
 for i in xrange(q):
     query = int(raw_input())
     ind = (front + query) % n
